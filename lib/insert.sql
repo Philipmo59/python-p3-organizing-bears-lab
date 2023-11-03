511aa3d1f23286af14e03766b46ca8b60e1f5b2e
philipmo <philipmo59@gmail.com>
2023-11-02 21:40:51 -0400

    finished

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate",34,"M","Black","Happy",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",34,"M","Black","Happy",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",34,"M","Black","Happy",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",34,"M","Black","Happy",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",34,"M","Black","Happy",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",34,"M","Black","Happy",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",34,"M","Black","Happy",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,34,"M","Black","Happy",1);
